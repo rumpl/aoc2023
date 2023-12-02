99b3166c85cbe6c7e7a3f60be87929a514f587db
Djordje Lukic <djordje.lukic@docker.com>
2023-12-02 18:09:17 +0100

    day02 part02
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

--- day02/main.zig
+++ day02/main.zig
@@ -8,18 +8,17 @@ pub fn main() !void {
     var total: u32 = 0;
 
     while (splits.next()) |line| {
-        var impossible = false;
         var line_parts = split(u8, line, ":");
-        var first = line_parts.next().?;
+        _ = line_parts.next().?;
         var second = line_parts.next().?;
-        var game_id_parts = split(u8, first, " ");
-        _ = game_id_parts.next();
-        var game_id = game_id_parts.next().?;
-        var gi = try std.fmt.parseInt(u32, game_id, 10);
 
         var games = split(u8, second, ";");
+        var min_r: u32 = 0;
+        var min_b: u32 = 0;
+        var min_g: u32 = 0;
+        var my_hash_map = std.StringHashMap(u32).init(allocator);
+
         while (games.next()) |game| {
-            var my_hash_map = std.StringHashMap(u32).init(allocator);
             var colors = split(u8, game, ",");
             while (colors.next()) |color| {
                 var color_parts = split(u8, color, " ");
@@ -29,24 +28,24 @@ pub fn main() !void {
                 var color_name = color_parts.next().?;
 
                 var cv = try std.fmt.parseInt(u32, color_value, 10);
-                var toto = my_hash_map.get(color_name);
 
-                if (toto) |v| {
-                    try my_hash_map.put(color_name, v + cv);
-                } else {
-                    try my_hash_map.put(color_name, cv);
-                }
+                try my_hash_map.put(color_name, cv);
             }
             var red = my_hash_map.get("red") orelse 0;
             var blue = my_hash_map.get("blue") orelse 0;
             var green = my_hash_map.get("green") orelse 0;
-            if (red > 12 or blue > 14 or green > 13) {
-                impossible = true;
+
+            if (red > min_r) {
+                min_r = red;
+            }
+            if (blue > min_b) {
+                min_b = blue;
+            }
+            if (green > min_g) {
+                min_g = green;
             }
         }
-        if (!impossible) {
-            total += gi;
-        }
+        total += (min_r * min_b * min_g);
     }
     std.debug.print("{d}\n", .{total});
 }
