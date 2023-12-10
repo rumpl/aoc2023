1b99d7ac21311c26cd7dde2a01ea4ce957ba9eff
Djordje Lukic <djordje.lukic@docker.com>
2023-12-10 23:13:08 +0100

    day07 part 1

---
+++ day07/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func countCards(cards string) map[string]int {
+	m := make(map[string]int)
+	for _, card := range cards {
+		m[string(card)]++
+	}
+	return m
+}
+
+func rank(cards string) int {
+	c := countCards(cards)
+
+	// five of a kind
+	if len(c) == 1 {
+		return 7
+	}
+	if len(c) == 2 {
+		for _, v := range c {
+			// 4 of a kind
+			if v == 4 {
+				return 6
+			}
+			if v == 3 {
+				for _, v := range c {
+					// full house
+					if v == 2 {
+						return 5
+					}
+				}
+			}
+		}
+	}
+
+	// two pair
+	if len(c) == 3 {
+		for _, v := range c {
+			// three of a kind
+			if v == 3 {
+				return 4
+			}
+		}
+		// two pair
+		return 3
+	}
+
+	// one pair
+	if len(c) == 4 {
+		return 2
+	}
+	// high card
+	return 1
+}
+
+type hand struct {
+	Cards string
+	Bid   int
+}
+
+func main() {
+	b, _ := os.ReadFile("input.txt")
+	data := string(b)
+	lines := strings.Split(data, "\n")
+	hands := []hand{}
+	for _, line := range lines {
+		cards := strings.Split(line, " ")
+		n, _ := strconv.Atoi(cards[1])
+		hands = append(hands, hand{Cards: cards[0], Bid: n})
+	}
+
+	slices.SortFunc[[]hand](hands, func(i, j hand) int {
+		irank := rank(i.Cards)
+		jrank := rank(j.Cards)
+		if irank > jrank {
+			return 1
+		}
+		if irank < jrank {
+			return -1
+		}
+		s := map[byte]int{
+			'T': 10,
+			'J': 11,
+			'Q': 12,
+			'K': 13,
+			'A': 14,
+			'2': 2,
+			'3': 3,
+			'4': 4,
+			'5': 5,
+			'6': 6,
+			'7': 7,
+			'8': 8,
+			'9': 9,
+		}
+		for k := 0; k < 5; k++ {
+			if s[i.Cards[k]] > s[j.Cards[k]] {
+				return 1
+			}
+			if s[i.Cards[k]] < s[j.Cards[k]] {
+				return -1
+			}
+		}
+		return 0
+	})
+
+	total := 0
+	for i := 0; i < len(hands); i++ {
+		total += hands[i].Bid * (i + 1)
+	}
+	fmt.Println(total)
+}
