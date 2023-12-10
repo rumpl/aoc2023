49db936cc445446615f8fd0b9cffd57edb272194
Djordje Lukic <djordje.lukic@docker.com>
2023-12-10 19:13:25 +0100

    day10 part 1
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

---
+++ day10/go.mod
@@ -0,0 +1,5 @@
+module day10
+
+go 1.21.4
+
+require github.com/yourbasic/graph v0.0.0-20210606180040-8ecfec1c2869
---
+++ day10/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/yourbasic/graph"
+)
+
+func vertex(x, y int, side int) int {
+	return x*side + y
+}
+
+func isVerticalDown(x, y int, lines []string) bool {
+	if x >= 0 && x < len(lines) && y >= 0 && y < len(lines[0]) {
+		return lines[x][y] == '|' || lines[x][y] == 'F' || lines[x][y] == '7'
+	}
+	return false
+}
+
+func isVerticalUp(x, y int, lines []string) bool {
+	if x >= 0 && x < len(lines) && y >= 0 && y < len(lines[0]) {
+		return lines[x][y] == '|' || lines[x][y] == 'J' || lines[x][y] == 'L'
+	}
+	return false
+}
+
+func isHorizontalLeft(x, y int, lines []string) bool {
+	if x >= 0 && x < len(lines) && y >= 0 && y < len(lines[0]) {
+		return lines[x][y] == '-' || lines[x][y] == 'J' || lines[x][y] == '7'
+	}
+	return false
+}
+
+func isHorizontalRight(x, y int, lines []string) bool {
+	if x >= 0 && x < len(lines) && y >= 0 && y < len(lines[0]) {
+		return lines[x][y] == '-' || lines[x][y] == 'L' || lines[x][y] == 'F'
+	}
+	return false
+}
+
+func main() {
+	b, _ := os.ReadFile("input.txt")
+	data := string(b)
+
+	lines := strings.Split(data, "\n")
+
+	side := len(lines)
+	g := graph.New(len(lines) * len(lines[0]))
+
+	s := 0
+	for y, line := range lines {
+		for x, c := range line {
+			if c == '|' {
+				// if y-1 >= 0 {
+				if isVerticalDown(y-1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y-1, x, side))
+				}
+				if isVerticalUp(y+1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				}
+				// if y+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				// }
+			}
+			if c == '-' {
+				if isHorizontalRight(y, x-1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				}
+				if isHorizontalLeft(y, x+1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				}
+				// if x-1 >= 0 {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				// }
+				// if x+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				// }
+			}
+			if c == 'L' {
+				if isVerticalDown(y-1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y-1, x, side))
+				}
+				if isHorizontalLeft(y, x+1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				}
+				// if y-1 >= 0 {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y-1, x, side))
+				// }
+				// if x+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				// }
+			}
+			if c == 'J' {
+				if isVerticalDown(y-1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y-1, x, side))
+				}
+				if isHorizontalRight(y, x-1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				}
+
+				// if y-1 >= 0 {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y-1, x, side))
+				// }
+				// if x-1 >= 0 {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				// }
+			}
+			if c == '7' {
+				if isHorizontalRight(y, x-1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				}
+				if isVerticalUp(y+1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				}
+				// if y+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				// }
+				// if x-1 >= 0 {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x-1, side))
+				// }
+			}
+			if c == 'F' {
+				if isHorizontalLeft(y, x+1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				}
+				if isVerticalUp(y+1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				}
+				// if y+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				// }
+				// if x+1 < side {
+				// 	g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				// }
+			}
+			if c == 'S' {
+				s = vertex(y, x, side)
+				if isHorizontalLeft(y, x+1, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y, x+1, side))
+				}
+				if isVerticalUp(y+1, x, lines) {
+					g.AddBoth(vertex(y, x, side), vertex(y+1, x, side))
+				}
+			}
+		}
+	}
+
+	// fmt.Println(g.String())
+	// fmt.Println(graph.Components(g))
+	max := 0
+	for _, c := range graph.Components(g) {
+		for _, n := range c {
+			if n == s {
+				if len(c) > max {
+					max = len(c)
+				}
+			}
+		}
+	}
+	fmt.Println(max / 2)
+}
